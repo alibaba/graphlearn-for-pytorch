aa66295c782046bfdf36b30d648f2870a48094b6
baoleai <baoleai01@gmail.com>
2023-04-26 14:24:46 +0800

    fix UnifiedTensor UT.

--- test/python/test_unified_tensor.py
+++ test/python/test_unified_tensor.py
@@ -39,18 +39,17 @@ class UnifiedTensorTestCase(unittest.TestCase):
   def _test_with_dtype(self, dtype: torch.dtype):
     cpu_tensor1 = torch.ones(128, 128, dtype=dtype)
     cpu_tensor2 = cpu_tensor1 * 2
-    cpu_tensor3 = cpu_tensor1 * 3
 
-    tensors = [cpu_tensor1, cpu_tensor2, cpu_tensor3]
-    tensor_devices = [0, 1, -1]
+    tensors = [cpu_tensor1, cpu_tensor2]
+    tensor_devices = [0, -1]
 
     unified_tensor = UnifiedTensor(0, dtype)
     unified_tensor.init_from(tensors, tensor_devices)
-    self.assertEqual(unified_tensor.shape, [128*3, 128])
-    input = torch.tensor([10, 20, 200, 210, 300, 310], dtype=torch.int64,
+    self.assertEqual(unified_tensor.shape, [128*2, 128])
+    input = torch.tensor([10, 20, 200, 210], dtype=torch.int64,
                          device= torch.device('cuda:0'))
     feature = torch.ones(2, 128, dtype=dtype, device=torch.device('cuda:0'))
-    expected_res = torch.cat((feature, feature*2, feature*3), 0)
+    expected_res = torch.cat((feature, feature*2), 0)
     res = unified_tensor[input]
     self.assertEqual(expected_res.dtype, res.dtype)
     self.assertTrue(tensor_equal_with_device(expected_res, res))
