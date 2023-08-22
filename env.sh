f2272450aba02bae79324b088148956dfd5c4333
jzb <jzb403834@alibaba-inc.com>
2023-08-22 10:43:09 +0800

    delete env.sh

--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-script_dir=$(dirname "$(realpath "$0")")
-SYS_LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
-export LD_LIBRARY_PATH=$SYS_LD_LIBRARY_PATH:${script_dir}/built/lib:$LD_LIBRARY_PATH
