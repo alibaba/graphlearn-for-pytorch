2dde6f7c3009805526886865693cd0ee5764890f
Hongyi ZHANG <zhy409824@alibaba-inc.com>
2023-09-27 14:52:55 +0800

    update

--- scripts/launch_container.sh
+++ scripts/launch_container.sh
@@ -9,6 +9,7 @@ DESTDIR=$3
 
 WITH_VINEYARD=${WITH_VINEYARD:-OFF}
 WITH_CUDA=${WITH_CUDA:-ON}
+MOUNT_VOLUME=${MOUNT_VOLUME:-TRUE}
 
 ret=0
 for i in $(seq 1 3); do
@@ -23,11 +24,20 @@ then
   exit $ret
 fi
 
-docker_args="-itd --name $JOB_NAME --shm-size=1g -e WITH_VINEYARD=$WITH_VINEYARD -e WITH_CUDA=$WITH_CUDA -v $GLT_ROOT_DIR:$DESTDIR"
+docker_args="-itd --name $JOB_NAME --shm-size=1g -e WITH_VINEYARD=$WITH_VINEYARD -e WITH_CUDA=$WITH_CUDA"
 
 if [ "$WITH_CUDA" = "ON" ]
 then
   docker_args+=" --gpus all"
 fi
 
-docker run $docker_args $IMAGE_NAME bash
+if [ "$MOUNT_VOLUME" = "TRUE" ]
+then
+  # Option 1: Using -v option
+  docker_args+=" -v $GLT_ROOT_DIR:$DESTDIR"
+  docker run $docker_args $IMAGE_NAME bash
+else
+  # Option 2: Using docker cp
+  docker run $docker_args $IMAGE_NAME bash
+  docker cp $GLT_ROOT_DIR $JOB_NAME:$DESTDIR
+fi
