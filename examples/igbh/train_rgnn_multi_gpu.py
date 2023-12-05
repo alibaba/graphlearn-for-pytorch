5e9bd713e8fec214ffa3a0c1212f787b73faf075
yunzhoul <elnifio0519@gmail.com>
2023-12-05 11:34:36 -0800

    add header

--- examples/igbh/train_rgnn_multi_gpu.py
+++ examples/igbh/train_rgnn_multi_gpu.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Alibaba Group Holding Limited. All Rights Reserved.
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
